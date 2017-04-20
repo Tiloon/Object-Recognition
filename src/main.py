00bd01a6f89af7df12c0c9c149203c500365465a
Hugo Rybinski <rybins_h@epita.fr>
2017-04-20 21:39:56 +0200

    add box to cans

--- src/main.py
+++ src/main.py
@@ -3,26 +3,25 @@ import cv2
 import itertools
 import numpy as np
 
+
 def main():
-    print('lol')
     imgPath = chooseImagePath()
     imgPathRef = chooseImagePathRef()
 
     img = cv2.imread(imgPath, 1)
     imgRef = cv2.imread(imgPathRef, 1)
-    print_image(img)
-    print_image(imgRef)
-    #randomTry(img)
+    # print_image(img)
+    # print_image(imgRef)
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    print_image(gray)
+    # print_image(gray)
     sift = cv2.xfeatures2d.SIFT_create()
     (skp, sd) = sift.detectAndCompute(img, None)
     print("#IMG kps: {}, descriptors: {}".format(len(skp), sd.shape))
     (tkp, td) = sift.detectAndCompute(imgRef, None)
     print("#REF  kps: {}, descriptors: {}".format(len(tkp), td.shape))
-    #cv2.drawKeypoints(gray, tkp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    #print_image(img)
+    # cv2.drawKeypoints(gray, tkp, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    # print_image(img)
 
     flann_params = dict(algorithm=1, trees=4)
     flann = cv2.flann.Index(sd, flann_params)
@@ -46,31 +45,122 @@ def main():
 
     print("#FINAL kps: {}".format(len(skp_final)))
     cv2.drawKeypoints(imgRef, tkp, imgRef, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    print_image(imgRef)
+    # print_image(imgRef)
     cv2.drawKeypoints(gray, skp_final, img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    print_image(img)
+    # print_image(img)
 
     printKeyDiff(img, imgRef, skp_final, tkp)
 
+    findBox(img, skp_final)
 
-    #lower_white = np.array([0, 0, 0])
-    #upper_white = np.array([0, 0, 255])
-    #myMask2 = cv2.inRange(imgHSV, lower_white, upper_white)
-    #print_image(myMask2)
+
+
+
+
+
+    # lower_white = np.array([0, 0, 0])
+    # upper_white = np.array([0, 0, 255])
+    # myMask2 = cv2.inRange(imgHSV, lower_white, upper_white)
+    # print_image(myMask2)
     # TODO: dans le mask, dilater + erosion pour trouver les carres noir
     # foutre des marqueurs dans les carres trouves
     # avec signature chercher les ecriture Minute maid? (chaud car arrondis)
 
 
-    #kernel = np.ones((3,3), np.uint8)
-    #Faire du close est une mauvaise idee, on va juste close entre le POMME en dessous et le rectangle
-    #n = 6
-    #myMaskEr = cv2.erode(myMask, kernel, iterations=n)
-    #print_image(myMaskEr)
-    #myMaskEr = cv2.dilate(myMaskEr, kernel, iterations=n)
-    #print_image(myMaskEr)
-    #res = cv2.bitwise_and(myMask, myMask, mask=myMaskEr)
-    #print_image(res)
+    # kernel = np.ones((3,3), np.uint8)
+    # Faire du close est une mauvaise idee, on va juste close entre le POMME en dessous et le rectangle
+    # n = 6
+    # myMaskEr = cv2.erode(myMask, kernel, iterations=n)
+    # print_image(myMaskEr)
+    # myMaskEr = cv2.dilate(myMaskEr, kernel, iterations=n)
+    # print_image(myMaskEr)
+    # res = cv2.bitwise_and(myMask, myMask, mask=myMaskEr)
+    # print_image(res)
+
+
+def findBox(img, skp_final):
+    xCenter, yCenter = findCenter(skp_final)
+    img2 = img.copy()
+    cv2.circle(img2, (xCenter, yCenter), 10, color('lb'), thickness=-1)
+    print_image(img2)
+    moyDist = 0
+    for kp in skp_final:
+        moyDist += np.math.sqrt((kp.pt[0] - xCenter) ** 2 + (kp.pt[1] - yCenter) ** 2)
+    moyDist = int(moyDist // len(skp_final))
+    skp_final_proper = skp_final.copy()
+    for kp in skp_final:
+        dist = np.math.sqrt((kp.pt[0] - xCenter) ** 2 + (kp.pt[1] - yCenter) ** 2)
+        if (dist < moyDist):
+            cv2.circle(img2, (int(kp.pt[0]), int(kp.pt[1])), 10, color('g'), thickness=2)
+        if (dist < moyDist * 2):
+            cv2.circle(img2, (int(kp.pt[0]), int(kp.pt[1])), 10, color('o'), thickness=2)
+        else:
+            skp_final_proper.remove(kp)
+            cv2.circle(img2, (int(kp.pt[0]), int(kp.pt[1])), 10, color('r'), thickness=-1)
+
+    print_image(img2)
+
+    xCenter, yCenter = findCenter(skp_final_proper)
+
+    img3 = img.copy()
+    cv2.circle(img3, (xCenter, yCenter), 10, color('lb'), thickness=-1)
+    print_image(img3)
+
+    if len(skp_final_proper) > 0:
+        maxX, maxY, minX, minY = skp_final_proper[0].pt[0], skp_final_proper[0].pt[1], skp_final_proper[0].pt[0], skp_final_proper[0].pt[1]
+        for kp in skp_final_proper:
+            maxX = max(maxX, kp.pt[0])
+            maxY = max(maxY, kp.pt[1])
+            minX = min(minX, kp.pt[0])
+            minY = min(minY, kp.pt[1])
+
+        cv2.rectangle(img3, (int(maxX), int(maxY)), (int(minX), int(minY)), color('o'), thickness=5)
+
+        w, h = maxX - minX, maxY - minY
+
+
+
+        if h > w:
+            print("swaping")
+            maxX, minX, maxY, minY = maxY, minY, maxX, minX
+            w, h = maxX - minX, maxY - minY
+
+
+        maxX, minX = maxX + w, minX - w
+        maxY, minY = maxY + 5 * h, minY - 2 * h
+
+
+
+        cv2.rectangle(img3, (int(maxX), int(maxY)), (int(minX), int(minY)), color('r'), thickness=5)
+        print_image(img3)
+
+
+
+
+
+def findCenter(skp_final):
+    (xCenter, yCenter) = (0, 0)
+    for kp in skp_final:
+        (xCenter, yCenter) = (xCenter + kp.pt[0], yCenter + kp.pt[1])
+    (xCenter, yCenter) = (int(xCenter // len(skp_final)), int(yCenter // len(skp_final)))
+    print((xCenter, yCenter))
+    return xCenter, yCenter
+
+
+def color(code):
+    if code == "r": # or code == "red":
+        return rgb(196, 47, 39)
+    if code == "o":
+        return rgb(232, 121, 2)
+    if code == "g":
+        return rgb(43, 219, 8)
+    if code == "lb":
+        return rgb(2, 203, 206)
+    return (0, 0, 0)
+
+
+def rgb(r, g, b):
+    return (b, g, r)
 
 
 def printKeyDiff(img, imgRef, skp_final, tkp):
@@ -103,43 +193,45 @@ def randomTry(img):
     myMask = cv2.inRange(imgHSV, lower_black, upper_black)
     print_image(myMask)
 
+
 def chooseImagePathRef():
-    #TEST
-    #imgPath = "../resources/ref_canette.jpg"
+    # TEST
+    # imgPath = "../resources/ref_canette.jpg"
     imgPath = "../resources/ref_logo.jpg"
     return imgPath
 
+
 def chooseImagePath():
-    #TEST
-    #imgPath = "../resources/ref_canette.jpg"
-    #EASY
-    imgPath = "../resources/facile/20160524_163619.jpg"
-    #imgPath = "../resources/facile/20160525_143739.jpg"
-    #imgPath = "../resources/facile/20160525_144343.jpg"
-    #imgPath = "../resources/facile/20160525_145003.jpg"
-    #QUASIFACILE
-    #imgPath = "../resources/quasi_facile/20160506_125941.jpg" #DESSUS
-    #imgPath = "../resources/quasi_facile/20160525_143735.jpg" #DEVANT FENETRE
-    #imgPath = "../resources/quasi_facile/20160525_144433.jpg" #DISTORSION
-    #imgPath = "../resources/quasi_facile/20160506_130022.jpg" #FAIL
-    #imgPath = "../resources/quasi_facile/20160525_143754.jpg"
-    #imgPath = "../resources/quasi_facile/20160525_144511.jpg"
-    #imgPath = "../resources/quasi_facile/20160515_165208.jpg" #FAIL
-    #imgPath = "../resources/quasi_facile/20160525_144248.jpg" #BOULE ANTISTRESS
-    #imgPath = "../resources/quasi_facile/20160525_144527.jpg" #OMBRE
-    #imgPath = "../resources/quasi_facile/20160515_165214.jpg" #FAIL
-    #imgPath = "../resources/quasi_facile/20160525_144412.jpg" #NATURE
-    #imgPath = "../resources/quasi_facile/20160525_144533.jpg"
-    #imgPath = "../resources/quasi_facile/20160523_190950.jpg"
-    #imgPath = "../resources/quasi_facile/20160525_144416.jpg"
-    #imgPath = "../resources/quasi_facile/20160525_145157.jpg"
-    #imgPath = "../resources/quasi_facile/20160523_191142.jpg"
-    #imgPath = "../resources/quasi_facile/20160525_144422.jpg"
-    #imgPath = "../resources/quasi_facile/photo_mm.jpg"
-    #imgPath = "../resources/quasi_facile/20160523_191210.jpg"
-    #imgPath = "../resources/quasi_facile/20160525_144427.jpg"
-    #imgPath = "../resources/quasi_facile/20160523_191304.jpg"
-    #imgPath = "../resources/quasi_facile/20160525_144430.jpg"
+    # TEST
+    # imgPath = "../resources/ref_canette.jpg"
+    # EASY
+    # imgPath = "../resources/facile/20160524_163619.jpg"
+    # imgPath = "../resources/facile/20160525_143739.jpg" # normal devant fenetre
+    # imgPath = "../resources/facile/20160525_144343.jpg" # 90°
+    # imgPath = "../resources/facile/20160525_145003.jpg"
+    # QUASIFACILE
+    # imgPath = "../resources/quasi_facile/20160506_125941.jpg" #DESSUS
+    # imgPath = "../resources/quasi_facile/20160525_143735.jpg" #DEVANT FENETRE
+    # imgPath = "../resources/quasi_facile/20160525_144433.jpg" #DISTORSION
+    # imgPath = "../resources/quasi_facile/20160506_130022.jpg" #FAIL
+    # imgPath = "../resources/quasi_facile/20160525_143754.jpg"
+    # imgPath = "../resources/quasi_facile/20160525_144511.jpg"
+    # imgPath = "../resources/quasi_facile/20160515_165208.jpg" #FAIL
+    # imgPath = "../resources/quasi_facile/20160525_144248.jpg" #BOULE ANTISTRESS
+    # imgPath = "../resources/quasi_facile/20160525_144527.jpg" #OMBRE
+    # imgPath = "../resources/quasi_facile/20160515_165214.jpg" #FAIL
+    imgPath = "../resources/quasi_facile/20160525_144412.jpg" #NATURE
+    # imgPath = "../resources/quasi_facile/20160525_144533.jpg"
+    # imgPath = "../resources/quasi_facile/20160523_190950.jpg"
+    # imgPath = "../resources/quasi_facile/20160525_144416.jpg"
+    # imgPath = "../resources/quasi_facile/20160525_145157.jpg"
+    # imgPath = "../resources/quasi_facile/20160523_191142.jpg"
+    # imgPath = "../resources/quasi_facile/20160525_144422.jpg" # HARD
+    # imgPath = "../resources/quasi_facile/photo_mm.jpg"
+    # imgPath = "../resources/quasi_facile/20160523_191210.jpg"
+    # imgPath = "../resources/quasi_facile/20160525_144427.jpg"
+    # imgPath = "../resources/quasi_facile/20160523_191304.jpg"
+    # imgPath = "../resources/quasi_facile/20160525_144430.jpg"
     print(imgPath)
     return imgPath
 
